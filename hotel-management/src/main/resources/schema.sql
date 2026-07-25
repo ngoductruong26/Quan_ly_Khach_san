bab0a9b9cad56a4253c2b6dda5f79008f9814832
ngoductruong26 <ngoductruong397@gmail.com>
2026-07-25 08:31:41 +0700

    Update schema.sql

--- hotel-management/src/main/resources/schema.sql
+++ hotel-management/src/main/resources/schema.sql
@@ -1,2 +1,7 @@
 CREATE DATABASE IF NOT EXISTS quan_ly_khach_san;
-USE quan_ly_khach_san;
+USE quan_ly_khach_san
+CREATE TABLE dich_vu (
+                         dich_vu_id INT AUTO_INCREMENT PRIMARY KEY,
+                         loai_dich_vu VARCHAR(100) NOT NULL,
+                         gia_dich_vu DECIMAL(15,2) NOT NULL
+);
