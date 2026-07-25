e7ae7919ee31bcf8e5366ec74df57d2c7c5de0ab
ngoductruong26 <ngoductruong397@gmail.com>
2026-07-25 08:42:16 +0700

    Initial commit

--- hotel-management/src/main/resources/schema.sql
+++ hotel-management/src/main/resources/schema.sql
@@ -5,3 +5,22 @@ CREATE TABLE dich_vu (
                          loai_dich_vu VARCHAR(100) NOT NULL,
                          gia_dich_vu DECIMAL(15,2) NOT NULL
 );
+CREATE TABLE khach_hang (
+                            khach_hang_id INT AUTO_INCREMENT PRIMARY KEY,
+                            ten_khach_hang VARCHAR(50) NOT NULL,
+                            ho_khach_hang VARCHAR(50) NOT NULL,
+                            dia_chi TEXT,
+                            ngay_sinh DATE,
+                            email VARCHAR(100)
+);
+CREATE TABLE nhan_vien (
+                           nhan_vien_id INT AUTO_INCREMENT PRIMARY KEY,
+                           ten_nhan_vien VARCHAR(50) NOT NULL,
+                           ho_nhan_vien VARCHAR(50) NOT NULL,
+                           dia_chi TEXT,
+                           phong_ban VARCHAR(50),
+                           chuc_vu VARCHAR(50),
+                           ten_dang_nhap VARCHAR(50) UNIQUE,
+                           mat_khau VARCHAR(255)
+);
+
